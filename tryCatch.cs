cd6edf19e06f344b26b8ec92ff7cd9e0fdff4439
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-05 14:21:49 +0300

    Create tryCatch.cs

---
+++ tryCatch.cs
@@ -0,0 +1,37 @@
+using System;
+
+  namespace tryCatch
+  {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+       
+       
+       
+       
+       try
+            {
+
+                Console.WriteLine("Birinci sayiyi giriniz:");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("İkinci sayiyi giriniz:");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(number1 + number2);
+            }
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("sayi= 1 2 3 4 5 6 7 8 9 falan girmen lazım sacmalama");
+
+            }
+            
+            
+      }
+    }
+}
