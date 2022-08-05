8831f6feaaa7d70990c26aa39967b03d5b3503b4
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-05 13:35:29 +0300

    Create dortgenAlanHesap.cs

---
+++ dortgenAlanHesap.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+
+
+            #region DataCasting
+            //konsol üzerinden küçük kenarı ve büyük kenarı girilen bir dikdörtgenin çevresini hesaplayan uygulamayı yazınız.
+
+            Console.WriteLine("uzun kenarı giriniz");
+            string uzunKenar = Console.ReadLine();
+
+
+            Console.WriteLine("kısa kenarı giriniz");
+            string kisaKenar = Console.ReadLine();
+            //int x = Convert.ToInt32(uzunKenar); ToInt64=float ToInt16=Short ToInt32=Int
+            // int x = int.Parse(uzunKenar);
+            // int y = int.Parse(kisaKenar);
+            double x = double.Parse(uzunKenar);
+            double y = double.Parse(kisaKenar);
+            double alan = x * y;
+            Console.WriteLine(alan);
+
+
+            #endregion
+        }
+    }
+}
