be5f17018e0705c8d537aafc976245af4f1265fa
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-05 14:12:56 +0300

    Create numberGuessingGame.cs

---
+++ numberGuessingGame.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        
+        
+         Console.WriteLine("Bir sayi tutun ve enter'a basın");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Tuttugunuz sayiyi iki ile carp ve entera bas");
+            Console.ReadLine();
+
+            Console.WriteLine("Buldugun sonucu belirledigin ikinci bir sayi ile topla ve bu ikinci sayiyi ekrana yaz, enterla.");
+            double number = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Buldugun sonucu ikiye bol ve entera bas");
+            Console.ReadLine();
+            
+            Console.WriteLine("Buldugun sonuctan ilk tuttugun sayiyi cikar ve entera bas");
+            Console.ReadLine();
+            
+            string tahmin = (number / 2).ToString();
+            
+            Console.WriteLine("Tahmin ediyorum, buldugun sayi ="+tahmin+"mi/mu?");
+            Console.ReadLine();
+
+
+          
+          
+        }
+    }
+}
